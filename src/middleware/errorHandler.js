8ee3b25ff300051456b1c2d3aa4eab7c26082cbf
Chris Greg <christickhubs@gmail.com>
2026-05-12 10:00:10 +0000

    update globalErrorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -14,7 +14,7 @@ export const globalErrorHandler = (err, req, res, next) => {
 
     if (err.name === "ValidationError") {
         const message = Object.values(err.errors).map(val => val.message);
-        return res.status(4000).json({ success: false, error: message });
+        return res.status(400).json({ success: false, error: message });
     }
 
     console.error("CRASH DETAILS:", err);
